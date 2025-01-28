924e9c924b088982a1f10471b9d34956a4f97517
Miguel.Ruiz <miguel.ruiz@ENCEDCCUULT0468.local>
2025-01-27 21:40:06 -0600

    Search Bar w/filters

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -14,17 +14,18 @@ import { Label } from "./ui/label";;
   import { useState, useEffect } from "react";
 import { Input } from "./ui/input";
 
-export default function SearchBar() {
-  const [priority, setPriority]= useState("All")
-  const [state, setState]= useState("All")
+export default function SearchBar({onSearch}) {
+  const [priority, setPriority]= useState("")
+  const [state, setState]= useState("")
+  const [name, setName]= useState("")
 
     return (
-        <div className="bg-red-900">
-            <div className="flex ">            
-            <Label htmlFor="text" className="text-xl text-white">Name</Label>
-            <Input type="text" placeholder="text" className="ml-2 p-2 text-center rounded-md bg-white"/>
+        <div className="border border-gray-300 rounded-md justify-center">
+            <div className="flex w-1/4 ">            
+            <Label htmlFor="text" className="text-lg text-black">Name</Label>
+            <Input type="text" placeholder="Enter Name" className="ml-2 p-2 text-center rounded-md bg-white" onChange={(e)=> setName(e.target.value)}/>
             </div>
-            <div className="flex flex-row bg-blue-700">
+            <div className="flex flex-row mt-2">
                 <div className="flex flex-col">
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
@@ -34,7 +35,7 @@ export default function SearchBar() {
                         <DropdownMenuLabel>Choose a Priority</DropdownMenuLabel>
                         <DropdownMenuSeparator />
                         <DropdownMenuRadioGroup value={priority} onValueChange={setPriority}>
-                        <DropdownMenuRadioItem value="All">All</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="">All</DropdownMenuRadioItem>
                         <DropdownMenuRadioItem value="High">High</DropdownMenuRadioItem>
                         <DropdownMenuRadioItem value="Medium">Medium</DropdownMenuRadioItem>
                         <DropdownMenuRadioItem value="Low">Low</DropdownMenuRadioItem>
@@ -49,14 +50,14 @@ export default function SearchBar() {
                         <DropdownMenuLabel>Choose a State</DropdownMenuLabel>
                         <DropdownMenuSeparator />
                         <DropdownMenuRadioGroup value={state} onValueChange={setState}>
-                        <DropdownMenuRadioItem value="All">All</DropdownMenuRadioItem>
-                        <DropdownMenuRadioItem value="Done">Done</DropdownMenuRadioItem>
-                        <DropdownMenuRadioItem value="UnDone">UnDone</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="">All</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="True">Done</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="False">UnDone</DropdownMenuRadioItem>
                         </DropdownMenuRadioGroup>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
-                <Button size='lg' className="flex bg-slate-200 text-black justify-end">Search</Button>
+                <Button size='lg' className="flex bg-slate-200 text-black justify-end" onClick={()=>onSearch(name, priority, state)}>Search</Button>
             </div>
         </div>
     )
