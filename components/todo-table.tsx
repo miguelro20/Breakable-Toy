674f41ed6274fbd0a9072a75c9d6ea7afe5e6e7e
Miguel.Ruiz <miguel.ruiz@ENCEDCCUULT0468.local>
2025-01-27 23:42:07 -0600

    Checkbox done/undone controls

--- components/todo-table.tsx
+++ components/todo-table.tsx
@@ -2,7 +2,6 @@ import { ToDo } from "@/app/interfaces/to-do"
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableFooter,
     TableHead,
@@ -10,9 +9,10 @@ import {
     TableRow,
   } from "@/components/ui/table"
 import { Button } from "./ui/button"
-import { Check, ChevronLeft, ChevronRight, X} from "lucide-react"
+import { ChevronLeft, ChevronRight, X} from "lucide-react"
 import { useState } from "react"
 import { Label } from "./ui/label"
+import { Checkbox } from "./ui/checkbox"
   
 interface ToDoTableProps{
   toDos:ToDo[], 
@@ -20,7 +20,6 @@ interface ToDoTableProps{
   currentPage: number, 
   totalPages: number
 }
-
   export function ToDoTable({toDos, onPageChange, currentPage, totalPages}: ToDoTableProps) {
 
     const [isOpen, setIsOpen]= useState(false)
@@ -47,6 +46,48 @@ interface ToDoTableProps{
     };
 
 
+    const handleToDoDone = async (id: number) => {
+      try {
+        const response = await fetch(`http://localhost:9090/api/done/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        console.log(response)
+  
+        if (!response.ok) {
+          throw new Error('Failed to update status');
+        }
+        alert('Great, you completed a task!!');
+        window.location.reload();
+      } catch (error) {
+        console.error('Error:', error);
+        alert('Error updating status');
+      }
+    };
+
+    const handleToDoUnDone = async (id: number) => {
+      try {
+        const response = await fetch(`http://localhost:9090/api/undone/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        console.log(response)
+  
+        if (!response.ok) {
+          throw new Error('Failed to update status');
+        }
+        alert('Status Updated');
+        window.location.reload();
+      } catch (error) {
+        console.error('Error:', error);
+        alert('Error updating status');
+      }
+    };
+
     console.log('this is the to do data', toDos)
     return (
       <div>
@@ -58,16 +99,18 @@ interface ToDoTableProps{
             <TableHead>Name</TableHead>
             <TableHead>Priority</TableHead>
             <TableHead className="text-right">Due Date</TableHead>
+            <TableHead>Done Date</TableHead>
             <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {toDos.map((toDo:ToDo ) => (
             <TableRow key={toDo.id}>
-              <TableCell>{toDo.status ? <Check/> : <X/>}</TableCell>
+              <TableCell>{toDo.status ? <Checkbox checked onCheckedChange={()=>handleToDoUnDone(toDo.id)}/> : <Checkbox onCheckedChange={()=>{if(!toDo.status){handleToDoDone(toDo.id)}}}/>}</TableCell>
               <TableCell>{toDo.name}</TableCell>
               <TableCell>{toDo.priority}</TableCell>
               <TableCell className="text-right">{toDo.dueDate}</TableCell>
+              <TableCell>{toDo.doneDate ? <div>{toDo.doneDate}</div>: <X/>}</TableCell>
               <TableCell className="items-start">
                 <Button variant={"link"} >Update</Button>
                 {/* {isOpen &&
