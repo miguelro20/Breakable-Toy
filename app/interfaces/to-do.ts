e903cde1681cdc4df8b6e3cd451ef56ff0fded2e
Miguel.Ruiz <miguel.ruiz@ENCEDCCUULT0468.local>
2025-01-27 23:45:25 -0600

    ToDo object interface

---
+++ app/interfaces/to-do.ts
@@ -0,0 +1,10 @@
+export interface ToDo {
+    id: number;
+    name:string;
+    description:string;
+    priority:string;
+    status:string;
+    dueDate: string;
+    doneDate: string;
+    creationDate:string;
+  }
